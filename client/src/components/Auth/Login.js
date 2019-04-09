95a0150c0a48e870e721da355f6fd2969b05d26e
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-04-09 13:25:26 +0300

    add GoogleLogin

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = (googleUser) => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log("Google user", idToken);
+  };
+
+  return (
+    <GoogleLogin
+      isSignedIn={true}
+      onSuccess={onSuccess}
+      clientId="893433774168-fggepcpja3e98mmlphp70b06m250h01n.apps.googleusercontent.com"
+    />
+  );
 };
 
 const styles = {
