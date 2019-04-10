c8c7f34e9155e3161f9ef0ccb620cd84f6bd4d40
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-04-10 11:39:16 +0300

    add react-map-gl and mapbox

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGL, { NavigationControl } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 55.755826,
+  longitude: 37.6173,
+  zoom: 13
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiYWxleGsxbmciLCJhIjoiY2p1YXh6ZXdzMDc1MzQ0bWl5Nmt0NmFidCJ9.Mlb5if_o696XwJFClyxI5w"
+        {...viewport}
+        onViewportChange={(newViewport) => setViewport(newViewport)}
+      >
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={(newViewport) => setViewport(newViewport)}
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import withRoot from "../withRoot";
 import Header from "../components/Header";
+import Map from "../components/Map";
 
 const App = () => {
-  return <Header />;
+  return (
+    <React.Fragment>
+      <Header />
+      <Map />
+    </React.Fragment>
+  );
 };
 
 export default withRoot(App);
